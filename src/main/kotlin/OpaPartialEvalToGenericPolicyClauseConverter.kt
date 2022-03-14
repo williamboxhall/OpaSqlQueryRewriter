fb3886ec5e8dbab23c9fe384f8b0f9cbbdfaa8cf
William Boxhall <william.boxhall@cultureamp.com>
2022-03-14 17:47:52 +1100

    opa AND clauses

--- src/main/kotlin/OpaPartialEvalToGenericPolicyClauseConverter.kt
+++ src/main/kotlin/OpaPartialEvalToGenericPolicyClauseConverter.kt
@@ -1,11 +1,11 @@
 class OpaPartialEvalToGenericPolicyClauseConverter {
     val clauseRegex = Regex("""\w+\.\w+ = "\w+"""") // pet.owner = "alice"
-    fun convert(opaPartialEvalResponse: String): Map<String, Pair<String, String>> {
-        val fragments = opaPartialEvalResponse.split("\n")
+    fun convert(opaPartialEvalResponse: String): Map<String, List<Pair<String, String>>> {
+        val fragments = opaPartialEvalResponse.split("\n").map { it.filter { it != ';' } }
         return fragments
             .filter { clauseRegex.matches(it) }
             .map { it.split(".", " = ") }
             .map { it[0] to (it[1] to it[2].filter { it != '\"' }) }
-            .toMap()
+            .groupBy { it.first }.mapValues { it.value.map { it.second } }
     }
 }
--- src/test/kotlin/OpaPartialEvalToGenericPolicyClauseConverterTest.kt
+++ src/test/kotlin/OpaPartialEvalToGenericPolicyClauseConverterTest.kt
@@ -5,12 +5,23 @@ internal class OpaPartialEvalToGenericPolicyClauseConverterTest {
     val converter = OpaPartialEvalToGenericPolicyClauseConverter()
 
     @Test
-    fun `can parse basic opa partial responses`() {
+    fun `can parse single clause opa partial responses`() {
         val basicResponse = """
             # Query 1
             pet = data.pets[_];
             pet.owner = "alice"
         """.trimIndent()
-        assertEquals(mapOf("pet" to ("owner" to "alice")), converter.convert(basicResponse))
+        assertEquals(mapOf("pet" to listOf(("owner" to "alice"))), converter.convert(basicResponse))
+    }
+
+    @Test
+    fun `can parse AND opa partial responses`() {
+        val basicResponse = """
+            # Query 1
+            pet = data.pets[_];
+            pet.owner = "alice";
+            pet.name = "fluffy"
+        """.trimIndent()
+        assertEquals(mapOf("pet" to listOf(("owner" to "alice"), ("name" to "fluffy"))), converter.convert(basicResponse))
     }
 }
